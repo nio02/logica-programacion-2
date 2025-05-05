2238921d43f72463a413ec934d9c76758225f0e3
NICO <annicolas2@gmail.com>
2025-05-05 11:26:39 -0500

    Commit inicial

---
+++ script.js
@@ -0,0 +1,36 @@
+//Lógica-programación 2
+
+const gradosCelsius = document.getElementById("grados");
+const botonConvertir = document.getElementById("button");
+const contenedor = document.getElementById("container");
+
+function covertirFarenheit(grados){
+    let valor = ((grados) * 9 / 5) + 32;
+    return valor;
+};
+
+function convertirKelvin(grados){
+    let valor = (grados + 273.15);
+    return valor;
+};
+
+botonConvertir.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const valorCelsius = Number(gradosCelsius.value);
+
+    if (isNaN(valorCelsius)){ //¿is not a number? Devuelve false si es un número, devuelve true si NO es un número
+        alert("Por favor ingresa un número");
+        return;
+    }
+
+    const kelvin = document.createElement('p');
+    kelvin.textContent = `Grados Kelvin: ${convertirKelvin(valorCelsius)}`;
+
+    const farenheit = document.createElement('p');
+    farenheit.textContent = `Grados Fahrenheit: ${covertirFarenheit(valorCelsius)}`;
+
+    contenedor.appendChild(kelvin);
+    contenedor.appendChild(farenheit);
+
+})
